9506610a0c7a75fb94a50d203d2dbdafb82aeb8b
flamylight <y.dmytrenko11@gmail.com>
2025-12-24 14:53:50 +0200

    Add Task entity

---
+++ TaskManagerApp/Models/Task.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagerApp.Models;
+
+public class Task
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Please enter the name of the task")]
+    public string Title { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Please enter the description of the task")]
+    [StringLength(100, MinimumLength = 5,
+        ErrorMessage = "Description must be between 5 and 100 characters")]
+    public string Description { get; set; } = string.Empty;
+
+    public bool IsCompleted { get; set; }
+}
