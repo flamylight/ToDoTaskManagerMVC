073608554675939a999c8ca2e268fd4ac65446cc
flamylight <y.dmytrenko11@gmail.com>
2025-12-24 15:00:43 +0200

    Add ApllicationDbContext

---
+++ TaskManagerApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+namespace TaskManagerApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext: DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+        
+    }
+    
+    public DbSet<Task> Tasks { get; set; }
+}
