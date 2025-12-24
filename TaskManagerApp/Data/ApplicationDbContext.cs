1d1776a38da33deb063d1a940ad3b990bbf7bbff
flamylight <y.dmytrenko11@gmail.com>
2025-12-24 15:55:58 +0200

    Add EF Core migrations

--- TaskManagerApp/Data/ApplicationDbContext.cs
+++ TaskManagerApp/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 namespace TaskManagerApp.Data;
 using Microsoft.EntityFrameworkCore;
+using Models;
 
 public class ApplicationDbContext: DbContext
 {
@@ -10,4 +11,25 @@ public class ApplicationDbContext: DbContext
     }
     
     public DbSet<Task> Tasks { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Task>().HasData(
+            new Task
+            {
+                Id = 1,
+                Title = "test1",
+                Description = "Description 1",
+                IsCompleted = false,
+            },
+            new Task
+            {
+                Id = 2,
+                Title = "test2",
+                Description = "Test 2",
+                IsCompleted = true,
+            });
+    }
 }
---
+++ TaskManagerApp/Migrations/20251224133225_InitialCreate.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TaskManagerApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    IsCompleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tasks",
+                columns: new[] { "Id", "Description", "IsCompleted", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Description 1", false, "test1" },
+                    { 2, "Test 2", true, "test2" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
